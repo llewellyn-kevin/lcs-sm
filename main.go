fba906afdfcfbf0add6cf29dbd2a6f4e69f48378
Kevin Llewellyn <llewellynkevin1@gmail.com>
2020-02-08 23:08:44 -0700

    Basic formatting crap

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func getTeam(c *gin.Context) {
-	team_id := c.Param("team_id")
+func Getteam(c *gin.Context) {
+	TeamID := c.Param("team_id")
 
 	c.JSON(200, gin.H{
 		"name": "Cloud9",
-		"team_id": team_id,
+		"team_id": TeamID,
 		"current_value": "630",
 		"week1_value": "575",
 	})
@@ -27,7 +27,7 @@ func main() {
 	// API Version 1
 	v1 := router.Group("/v1")
 	{
-		v1.GET("/teams/:team_id", getTeam)
+		v1.GET("/teams/:team_id", Getteam)
 	}
 
 	router.Run(":8080")
