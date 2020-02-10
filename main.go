8fbcfc5711863de7c3910478dc5cfc0ba6f4edbe
Kevin Llewellyn <llewellynkevin1@gmail.com>
2020-02-10 00:28:55 -0700

    Setup Gorm with a simple database connection

--- main.go
+++ main.go
@@ -1,23 +1,54 @@
 package main
 
 import (
+	// "fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-func Getteam(c *gin.Context) {
-	TeamID := c.Param("team_id")
+var (
+	db *gorm.DB
+	connectionError error
+)
 
+type Team struct {
+	gorm.Model
+	Name string
+	CurrentValue int
+}
+
+func GetTeam(c *gin.Context) {
+	teamID := c.Param("team_id")
+	var team Team
+	db.First(&team, teamID)
+	
 	c.JSON(200, gin.H{
-		"name": "Cloud9",
-		"team_id": TeamID,
-		"current_value": "630",
-		"week1_value": "575",
+		"id": teamID,
+		"name": team.Name,
+		"value": team.CurrentValue,
 	})
 }
 
 func main() {
 	router := gin.Default()
+	dbname := "lcs_sm.db"
 
+	// Establish database connection
+	db, connectionError = gorm.Open("sqlite3", dbname)
+	if connectionError != nil {
+		panic(connectionError)
+	}
+	defer db.Close()
+
+	// TEMP: Seed the database manually
+	//db.AutoMigrate(&Team{})
+
+	//db.Create(&Team{Name: "Cloud9", CurrentValue: 605})
+	//db.Create(&Team{Name: "Team Liquid", CurrentValue: 580})
+
+	// Routing
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
@@ -27,7 +58,7 @@ func main() {
 	// API Version 1
 	v1 := router.Group("/v1")
 	{
-		v1.GET("/teams/:team_id", Getteam)
+		v1.GET("/teams/:team_id", GetTeam)
 	}
 
 	router.Run(":8080")
