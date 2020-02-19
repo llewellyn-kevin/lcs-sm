8c75f107654195550648ca651e8e9ce0090cccad
Kevin Llewellyn <llewellyn.kevin@yahoo.com>
2020-02-18 19:02:29 -0700

    Added split values to team view

--- web/components/team-split-info.js
+++ web/components/team-split-info.js
@@ -1,9 +1,29 @@
 Vue.component('team-split-info', {
   props: ['team', 'split'],
+  data: function() {
+    return {
+      stocks: [],
+    };
+  },
+  created: function() {
+    ax.get('/splits/' + this.split.ID + '/teams/' + this.team.ID + '/stock-values').then(response => {
+      this.stocks = response.data 
+    }).catch(error => {
+      console.log(error);
+      console.log(error.data);
+    });
+  },
   template: `
     <div>
       <h5>{{ split.League }} {{ split.Season }} {{ split.Year }}</h5>
-      <p>show stock data for {{ team.Name }}</p>
+      <ul class="list-group">
+        <li 
+          v-for="stock in stocks"
+          class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+          <strong>Week {{ stock.Week }}</strong>
+          <span>{{ stock.Value }}</span>
+        </li>
+      </ul>
     </div>
   `
 });
